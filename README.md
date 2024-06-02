b41888e52d8596b9c10acb434defe042bd9b026c
ayusudi <ayusudi.abc@gmail.com>
2024-06-02 22:34:03 +0700

    portofolio lucky shrub

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lucky-Shrub-Meta-Course
+
+![home page](./screenshots/home.png)
+![products](./screenshots/products.png)
+![about us](./screenshots/aboutus.png)
+![locations](./screenshots/locations.png)
---
+++ index.js
@@ -0,0 +1,184 @@
+let data = [
+  {
+    "id": 1,
+    "name": "European Silver Fir",
+    "image": "https://perenual.com/storage/species_image/1_abies_alba/regular/1536px-Abies_alba_SkalitC3A9.jpg",
+    "watering": "Frequent"
+  },
+  {
+    "id": 2,
+    "name": "Pyramidalis Silver Fir",
+    "image": "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 3,
+    "name": "White Fir",
+    "image": "https://perenual.com/storage/species_image/3_abies_concolor/regular/52292935430_f4f3b22614_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 4,
+    "name": "Candicans White Fir",
+    "image": "https://perenual.com/storage/species_image/4_abies_concolor_candicans/regular/49283844888_332c9e46f2_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 5,
+    "name": "Fraser Fir",
+    "image": "https://perenual.com/storage/species_image/5_abies_fraseri/regular/36843539702_e80fc436e0_b.jpg",
+    "watering": "Frequent"
+  },
+  {
+    "id": 6,
+    "name": "Golden Korean Fir",
+    "image": "https://perenual.com/storage/species_image/6_abies_koreana_aurea/regular/49235570926_99ec10781d_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 7,
+    "name": "Alpine Fir",
+    "image": "https://perenual.com/storage/species_image/7_abies_lasiocarpa/regular/51002756843_74fae3c2fa_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 8,
+    "name": "Blue Spanish Fir",
+    "image": "https://perenual.com/storage/species_image/8_abies_pinsapo_glauca/regular/21657514018_c0d9fed9f4_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 9,
+    "name": "Noble Fir",
+    "image": "https://perenual.com/storage/species_image/9_abies_procera/regular/49107504112_6bd7effb8b_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 10,
+    "name": "Johin Japanese Maple",
+    "image": "https://perenual.com/storage/species_image/10_acer_johin/regular/pexels-photo-2183508.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 11,
+    "name": "Snakebark Maple",
+    "image": "https://perenual.com/storage/species_image/11_acer_davidii/regular/6868591754_f4ac5b0510_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 12,
+    "name": "Amur Maple",
+    "image": "https://perenual.com/storage/species_image/12_acer_ginnala/regular/10476032513_76ca899bc4_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 13,
+    "name": "Flame Amur Maple",
+    "image": "https://perenual.com/storage/species_image/13_acer_ginnala_flame/regular/pexels-photo-1649190.jpg",
+    "watering": "Frequent"
+  },
+  {
+    "id": 14,
+    "name": "Red Rhapsody Amur Maple",
+    "image": "https://perenual.com/storage/species_image/14_acer_ginnala_mondy/regular/pexels-photo-1789879.jpg",
+    "watering": "Frequent"
+  },
+  {
+    "id": 15,
+    "name": "Ruby Slippers Amur Maple",
+    "image": "https://perenual.com/storage/species_image/15_acer_ginnala_ruby_slippers/regular/pexels-photo-1640820.jpg",
+    "watering": "Frequent"
+  },
+  {
+    "id": 16,
+    "name": "Paperbark Maple",
+    "image": "https://perenual.com/storage/species_image/16_acer_griseum/regular/5158906371_ed08a86876_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 17,
+    "name": "Fullmoon Maple",
+    "image": "https://perenual.com/storage/species_image/17_acer_japonicum/regular/7175475239_52543c51b3_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 18,
+    "name": "Cutleaf Fullmoon Maple",
+    "image": "https://perenual.com/storage/species_image/18_acer_japonicum_aconitifolium/regular/23528789198_c419363323_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 19,
+    "name": "Attaryi Fullmoon Maple*",
+    "image": "https://perenual.com/storage/species_image/19_acer_japonicum_attaryi/regular/pexels-photo-669323.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 20,
+    "name": "Golden Fullmoon Maple",
+    "image": "https://perenual.com/storage/species_image/20_acer_japonicum_aureum/regular/2560px-Acer_shirasawanum_27Aureum27.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 21,
+    "name": "Emmett's Pumpkin Fullmoon Maple",
+    "image": "https://perenual.com/storage/species_image/21_acer_japonicum_emmetts_pumpkin/regular/Acer_shirasawanum_28golden_fullmoon_maple29_2_284633285838129.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 22,
+    "name": "Green Cascade Maple",
+    "image": "https://perenual.com/storage/species_image/22_acer_japonicum_green_cascade/regular/4847225395_2509ee2bfe_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 23,
+    "name": "Big Leaf Maple",
+    "image": "https://perenual.com/storage/species_image/23_acer_macrophyllum/regular/52135137216_8a124b5188_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 24,
+    "name": "Mocha Rose Big Leaf Maple",
+    "image": "https://perenual.com/storage/species_image/24_acer_macrophyllum_mocha_rose/regular/4715169892_220a9d39f6_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 25,
+    "name": "Flamingo Boxelder",
+    "image": "https://perenual.com/storage/species_image/25_acer_negundo_flamingo/regular/5867345385_a9dff5bee7_b.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 26,
+    "name": "Kelly's Gold Boxelder",
+    "image": "https://perenual.com/storage/species_image/26_acer_negundo_kellys_gold/regular/28819730054_e2a2b797c9_b.jpg",
+    "watering": "Frequent"
+  },
+  {
+    "id": 27,
+    "name": "Japanese Maple",
+    "image": "https://perenual.com/storage/species_image/27_acer_palmatum/regular/2560px-Acer_palmatum_27Bloodgood27_kz01.jpg",
+    "watering": "Average"
+  },
+  {
+    "id": 28,
+    "name": "Emmett's Pumpkin Fullmoon Maple",
+    "image": "https://perenual.com/storage/species_image/21_acer_japonicum_emmetts_pumpkin/regular/Acer_shirasawanum_28golden_fullmoon_maple29_2_284633285838129.jpg",
+    "watering": "Average"
+  },
+]
+
+document.addEventListener("DOMContentLoaded", () => {
+  let content = document.getElementById("content-products");
+  data.forEach(el => {
+    let str = `
+      <div class="card">
+        <p><b>${el.name}</b></p>
+        <img src="${el.image}" class="imageproduct" />
+        <p>${el.watering} Watering</p>
+      </div>
+      `
+    content.innerHTML += str;
+  })
+})
